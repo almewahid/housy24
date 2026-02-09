5a5532c8e13518cd559934cfabb6303809465d45
almewahid <osakr100@gmail.com>
2026-02-09 12:07:41 +0200

    تحديث الموقع

---
+++ android/app/src/main/java/com/osama/housy24/MainActivity.java
@@ -0,0 +1,5 @@
+package com.osama.housy24;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.osama.housy24',
+  appName: 'housy24',
+  webDir: 'dist'
+};
+
+export default config;
